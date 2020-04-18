150765760775e4179d471247ca28565912d936c5
erik.baltazar123 <erik.baltazar123@gmail.com>
2020-04-18 18:29:04 -0500

    se modifica vista_menu dandole formato ok

--- encargado/vista/vista_menu.php
+++ encargado/vista/vista_menu.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Proyecto </title>
+	<title> Produccion </title>
 	<link rel="stylesheet" type="text/css" href="../../librerias/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../../librerias/bootstrap/css/bootstrap-theme.min.css">
 
@@ -15,12 +15,12 @@
 
 </head>
 <body>
-	<h1> Bienvenido al proyecto </h1>
+	<h1 align="center"> Distribuidora Rainbow Textil </h1>
 
 	<div class="row" style="margin: 0px; padding: 0px;"> 
 
-    <div class="col-lg-6 col-md-8 xs-12">
-    	<h3 align="center"> Registro de Datos </h3>
+    <div class="col-lg-4 col-md-6 xs-10">
+    	<h3 align="center"> Registro de Produccion </h3>
     	<div id="panel_registro" style="padding: 5%; box-shadow: 1px 2px 2px #A4A4A4; border:1px solid #A4A4A4;" align="center">
     		<!-- Panel de datos -->
     		<table class="table table-condensed" style="width: 70%;">
@@ -96,7 +96,7 @@
     	
     </div>
     <div class="col-lg-6 col-md-8 xs-12">
-    	<h3 align="center"> Listado de Datos </h3>
+    	<h3 align="center"> Datos Capturados </h3>
     	<button class="btn btn-info btn-md" onclick="btn_listar_datos();"> Listar </button>
     	<div id="panel_listado">
     		<!-- Panel de datos -->
