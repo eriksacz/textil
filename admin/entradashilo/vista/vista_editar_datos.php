7c39426da981d1b0f4782c83e6eebfc5f62df956
erik.baltazar123 <erik.baltazar123@gmail.com>
2020-05-27 22:37:23 -0500

    vista de ediatr ok con selects

--- admin/entradashilo/vista/vista_editar_datos.php
+++ admin/entradashilo/vista/vista_editar_datos.php
@@ -4,12 +4,23 @@ $id = $_POST['id'];
 
 require '../conector/conexion.php';
 
-$sql = mysqli_query($con,"SELECT * FROM altas WHERE id='$id'");
+$sql = mysqli_query($con,"SELECT altas.id,altas.no_factura,altas.no_pallets,altas.kilos,proveedores.proveedor, 
+composiciones.composicion,lotes.lote 
+from altas 
+INNER JOIN proveedores on altas.proveedor_id = proveedores.id 
+INNER JOIN composiciones on altas.composicion_id = composiciones.id 
+INNER JOIN lotes on altas.lote_id = lotes.id 
+WHERE altas.id='$id'");
+
+
 $row = mysqli_fetch_array($sql);
 
 $nofacatura = $row['no_factura']; 
 $nopallets = $row['no_pallets'];
 $kilos = $row['kilos'];
+$proveedor = $row['proveedor'];
+$composicion = $row['composicion'];
+$lote = $row['lote'];
 
 ?>
 <input type="hidden" id="id" value="<?php echo $id; ?>">
@@ -21,3 +32,39 @@ $kilos = $row['kilos'];
 
 <label for="kilos"> kilos </label>
 <input type="text" class="form-control" id="kilos_ed" placeholder="* kilos " value="<?php echo $kilos; ?>">
+
+
+                    <label for="inputState">Proveedor</label></td>
+					<select id="proveedor_ed" class="form-control">
+					<option selected><?php echo $proveedor; ?></option>
+					<?php
+					$sql = mysqli_query($con,"SELECT * FROM proveedores");
+					while($fila=$sql->fetch_array()){
+						echo "<option value='".$fila['proveedor']."'>".$fila['proveedor']."</option>";
+					}
+					?>
+					</select>
+
+                    
+                    <label for="inputState">Composicion</label></td>
+					<select id="composicion_ed" class="form-control">
+					<option selected><?php echo $composicion; ?></option>
+					<?php
+					$sql = mysqli_query($con,"SELECT * FROM composiciones");
+					while($fila=$sql->fetch_array()){
+						echo "<option value='".$fila['composicion']."'>".$fila['composicion']."</option>";
+					}
+					?>
+					</select>
+
+
+                    <label for="inputState">Lotes</label></td>
+					<select id="lote_ed" class="form-control">
+					<option selected><?php echo $lote; ?></option>
+					<?php
+					$sql = mysqli_query($con,"SELECT * FROM lotes");
+					while($fila=$sql->fetch_array()){
+						echo "<option value='".$fila['lote']."'>".$fila['lote']."</option>";
+					}
+					?>
+					</select>
