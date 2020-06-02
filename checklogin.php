0d81500d358a865c964d685d925f40927859d083
erik.baltazar123 <erik.baltazar123@gmail.com>
2020-06-02 17:14:58 -0500

    ya quedo checklogin

--- checklogin.php
+++ checklogin.php
@@ -31,39 +31,49 @@ if ($password==$row['password']) {
     $_SESSION['loggedin'] = true;
     $_SESSION['username'] = $username;
     $_SESSION['start'] = time();
-    $_SESSION['expire'] = $_SESSION['start'] + (20 * 60); //Aqui se le pone el tiempo que se desea por inactividad
+    $_SESSION['expire'] = $_SESSION['start'] + (1 * 60); //Aqui se le pone el tiempo que se desea por inactividad
 
     $sql1 = "SELECT rol_id,estado FROM $tbl_name WHERE username = '$username'";
+   
     $result1 = $conexion->query($sql1);
+   
     if ($result1->num_rows > 0) {     }
+   
     $row = $result1->fetch_array(MYSQLI_ASSOC);
-    if ($row['rol_id'] == 1 && $row['estado'] == 1) {
-          header('Location: admin/menu.php');//redirecciona a la pagina del usuario
-    }else if($row['rol_id'] == 2 && $row['estado'] == 1){
-          header('Location: encargado/vista/vista_menu.php');
-    } else {
-    ?>      
-   <!DOCTYPE html>
+    
+    if ($row['rol_id'] == 1) {
+        if($row['estado'] == 1){
+            header('Location: admin/menu.php');//redirecciona a la pagina del usuario
+        }    
+         
+    }else if($row['rol_id'] == 2){
+          if($row['estado'] == 1){
+            header('Location: encargado/vista/vista_menu.php');
+          }
+    }
+ 
+}else{
+?>
+<!DOCTYPE html>
    <html lang="en">
    <head>
+
          <meta charset="UTF-8">
          <meta name="viewport" content="width=device-width, initial-scale=1.0">
          <title>Document</title>
    </head>
    <body>
          <h1 align ="center">Usuario o contraseña incorrectos</h1>
-         <form align ="center" action="index.html">
+        <div class="container">
+        <form align ="center" action="index.html">
         <input type="submit" value="LOGIN" />
         </form>
-
+        </div>
+        
    </body>
    </html>
-   
-   
-   
-   
-   <?php
- }
- mysqli_close($conexion);
+
+<?php
 }
- ?>
+mysqli_close($conexion);
+?>
